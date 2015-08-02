63b33b05042ff02c077ef2619b1df872b229be44
Flash <lephuhai0511@gmail.com>
2015-08-03 01:12:55 +0700

    AJAX

---
+++ ajax/script.js
@@ -0,0 +1,15 @@
+/*
+	Description:
+	Documentation: https://xhr.spec.whatwg.org/
+*/
+
+var request;
+if (window.XMLHttpRequest) {
+	request = new XMLHttpRequest();
+} else {
+	request = new ActiveXObject('Microsoft.XMLHTTP');
+}
+request.open('GET','data.txt',false); // Async
+request.send();
+document.write(JSON.stringify(request));
+console.log(request);
